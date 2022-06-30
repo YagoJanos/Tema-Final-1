3b299ba7eb25b132f03f1b7c5b166f35f007f828
Yago Janos Andrevitch Gomes Quaranta <yjquaranta@gmail.com>
2022-06-30 11:59:08 -0300

    Delete KaryonRXNettyCalculator directory

--- KaryonRXNettyCalculator/src/main/java/com/github/yagojanos/cloud/exceptions/DivideByZeroException.java
+++
@@ -1,8 +0,0 @@
-package com.github.yagojanos.cloud.exceptions;
-
-public class DivideByZeroException extends RuntimeException{
-
-    public DivideByZeroException(){
-        super("You can't divide a number by zero");
-    }
-}
--- KaryonRXNettyCalculator/src/main/java/com/github/yagojanos/cloud/services/strategies/enums/OperationType.java
+++
@@ -1,9 +0,0 @@
-package com.github.yagojanos.cloud.services.strategies.enums;
-
-public enum OperationType {
-    ADDITION,
-    SUBTRACTION,
-    MULTIPLICATION,
-    DIVISION,
-    EXPONENTIATION
-}
